265a2a7f163b228f2e003e1b8d48fac0632cedea
roma564 <roma.gordiychuk.21@gmail.com>
2026-01-20 17:28:43 +0200

    quick fix src

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
-import { PrismaService } from 'src/prisma.service';
+import { PrismaService } from '../prisma.service';
 import { User } from 'generated/prisma';
 import { defaultModes } from '../mode/defaultModes/defaultModes';
 
