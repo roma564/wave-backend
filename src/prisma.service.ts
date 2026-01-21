ad94c29e4b528040cfd18737fbdc4e5862e1508d
roma564 <roma.gordiychuk.21@gmail.com>
2026-01-21 19:56:58 +0200

    prisma refector

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -1,10 +1,17 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
-
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  constructor() {
+    super(); // тепер конфіг береться з prisma.config.ts
+  }
+
   async onModuleInit() {
     await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
   }
 }
