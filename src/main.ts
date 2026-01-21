bd42e286461aedef7cf30af75bb3188390561ebf
roma564 <roma.gordiychuk.21@gmail.com>
2026-01-21 19:25:12 +0200

    fix: main.ts refactor

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import cookieParser from 'cookie-parser'
 
-import { NestExpressApplication } from '@nestjs/platform-express';
-import { join } from 'path';
-
-async function bootstrap() {
-  
-const app = await NestFactory.create<NestExpressApplication>(AppModule);
-
-  // app.enableCors();
-  
-  app.enableCors({
-  origin: true,
-  credentials: true,
-});
-
-
-
-  app.use(cookieParser());
-
-  
-  app.useStaticAssets(join(__dirname, '..', 'uploads'), {
-    prefix: '/uploads/', // URL-префікс
-  });
-  // await app.listen(5000, '192.168.0.105');
-  await app.listen(process.env.PORT ?? 5000);
+export default async function handler(req, res) {
+  const app = await NestFactory.create(AppModule);
+  await app.init();
+  const server = app.getHttpAdapter().getInstance();
+  return server(req, res);
 }
-bootstrap();
-
