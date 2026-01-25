e8e85083a08bcfa1afadc57a108a0a00ca9e3a41
roma564 <roma.gordiychuk.21@gmail.com>
2026-01-25 12:50:19 +0200

    prisma: pg adapter added

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -1,9 +1,20 @@
-import { Injectable } from "@nestjs/common";
-import { PrismaClient } from "@prisma/client";
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common'
+import { PrismaPg } from '@prisma/adapter-pg'
+import { PrismaClient } from '@prisma/client' 
 
 @Injectable()
-export class PrismaService extends PrismaClient {
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
   constructor() {
-    super(); // No need to pass adapter here
+    const connectionString = process.env.DATABASE_URL!
+    const adapter = new PrismaPg({ connectionString })
+    super({ adapter })  
+  }
+
+  async onModuleInit() {
+    await this.$connect()
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect()
   }
 }
