b8755d35774459ae31284e04e105ab8ccaa761b0
roma564 <roma.gordiychuk.21@gmail.com>
2026-02-04 17:43:29 +0200

    cors policy update

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,21 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: process.env.FRONTEND_URL,
+    origin: (origin, callback) => {
+      const allowedOrigins = [
+        process.env.FRONTEND_URL,
+        'http://localhost:3000',
+        'http://localhost:5173',
+      ].filter(Boolean);
+
+      if (!origin || allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+
+      return callback(new Error('Not allowed by CORS'));
+    },
     credentials: true,
   });
-
 
   await app.listen(process.env.PORT as string);
   console.log(` Server running on port ${process.env.PORT}`);
